447f11babdbfeb4d04a1f5dd7296ed0b733d96bd
Shyju Krishnankutty <connectshyju@gmail.com>
2019-08-29 15:53:04 -0700

    Adding the demo project

---
+++ sample/DemoApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace DemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public HomeController(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("headers")]
+        public string Headers()
+        {
+            var headers = this.httpContextAccessor.HttpContext.Request.Headers;
+            var requestHeader = string.Join(", ", headers.Select(a => a.Key + ":" + a.Value));
+
+            return requestHeader;
+        }
+    }
+}
