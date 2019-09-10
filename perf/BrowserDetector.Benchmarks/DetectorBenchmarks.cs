147d3433798cb8e0f80ca5865fa287c0ef1b8a38
Shyju <connectshyju@gmail.com>
2019-09-09 20:15:14 -0700

    Clean up

--- perf/BrowserDetector.Benchmarks/DetectorBenchmarks.cs
+++ perf/BrowserDetector.Benchmarks/DetectorBenchmarks.cs
@@ -6,7 +6,7 @@ namespace BrowserDetector.Benchmarks
 {
     [MemoryDiagnoser]
     public class DetectorBenchmarks
-    {
+    {        
         readonly string chromeWindows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36";
         readonly string safariWindows = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2";
         [Benchmark]
